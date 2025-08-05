acb28c26b75b867761c92c1cfb0d33e1af4bb82e
Valentina Garcia <valentinagarciag089@gmail.com>
2025-08-04 22:39:40 -0500

    Funciones para optimizar el menu

--- test_menu.cpp
+++ test_menu.cpp
@@ -4,9 +4,14 @@
 
 using namespace std;
 
+// Declaración de funciones necesarias para el menú
+void comandoAyuda(string argumento, int momento);
+void comandoNoReconocido();
+
+
 int main () {
     string linea, comando, argumento;
-
+    
     do{
         linea = "";
         comando = "";
@@ -43,74 +48,92 @@ int main () {
                     cout << "Las secuencias cargadas se guardaron en el archivo " << argumento << "\n";
                 }
                 else if(comando== "ayuda"){
-                    if(argumento.empty()){
-                        printf("Comandos disponibles: listar_secuencias, histograma, es_subsecuencia, enmascarar, guardar, salir, ayuda\n");
-                    }
-                    else{
-                        if(argumento == "listar_secuencias"){
-                            printf("Uso: listar_secuencias\n");
-                            printf("Descripción: imprime la cantidad de secuencias y su información.\n");
-                        }
-                        else if(argumento == "histograma"){
-                            printf("Uso: histograma <descripcion_secuencia>\n");
-                            printf("Descripción: imprime la frecuencia de cada código en la secuencia, definida por cada salto de linea.\n");
-                        }
-                        else if(argumento == "es_subsecuencia"){
-                            printf("Uso: es_subsecuencia <subsecuencia>\n");
-                            printf("Descripción: Determina si una subsecuencia existe dentro de una secuencia ya cargada.\n");
-                        }
-                        else if(argumento == "enmascarar"){
-                            printf("Uso: enmascarar <subsecuencia>\n");
-                            printf("Descripción: Enmascara una subsecuencia dada por el usuario si existe.\n");
-                        }
-                        else if(argumento == "guardar"){
-                            printf("Uso: guardar <nombre_archivo>\n");
-                            printf("Descripción: guarda en el archivo las secuencias cargadas en memoria.\n");
-                        }
-                        else if(argumento == "salir"){
-                            printf("Uso: salir\n");
-                            printf("Descripción: salida del programa.\n");
-                        }
-                    }
+                    comandoAyuda(argumento, 2);
                 }
                 else if (comando == "salir") {
                     break;
                 }
                 else{
-                    printf("Comando no reconocido. Escribe 'ayuda' para ver las opciones de comandos.\n");
-                    printf("Si necesitas ayuda con un comando especifico, escribe 'ayuda <comando>'\n");
+                    comandoNoReconocido();
                 }
             } while (comando != "salir");
             
         }
 
         else if(comando == "ayuda"){
-            if(argumento.empty()){
-                printf("Comandos disponibles: cargar, salir, ayuda\n");
-            }
-            else{
-                if(argumento == "cargar"){
-                    printf("Uso: cargar <ruta_archivo>\n");
-                    printf("Descripción: carga de archivo que contienen códigos genéticos.\n");
-                }
-                else if(argumento == "salir"){
-                    printf("so: salir\n");
-                    printf("Descripción: salida del programa.\n");
-                }
-                
-            }
+            comandoAyuda(argumento, 1);
         }
-
         else if (comando == "salir") {
             break;
         }
-        
         else{
-            printf("Comando no reconocido. Escribe 'ayuda' para ver las opciones de comandos.\n");
-            printf("Si necesitas ayuda con un comando especifico, escribe 'ayuda <comando>'\n");
+            comandoNoReconocido();
         }
-
     }while(comando != "salir");
 
     return 0;
+}
+
+
+
+
+// Ayudas para cada momento en el que el usuario se encuentre en el menú
+void comandoAyuda(string argumento, int momento) {
+    if(argumento == "salir"){
+        printf("Uso: salir\n");
+        printf("Descripción: salida del programa.\n");
+    }
+
+    else if(momento == 1) {
+        if(argumento.empty()){
+            printf("Comandos disponibles: cargar, salir, ayuda\n");
+        }
+        else{
+            if(argumento == "cargar"){
+                printf("Uso: cargar <ruta_archivo>\n");
+                printf("Descripción: carga de archivo que contienen códigos genéticos.\n");
+            }
+            else {
+                comandoNoReconocido();
+            }
+        }
+    }
+
+    else if(momento == 2){
+        if(argumento.empty()){
+            printf("Comandos disponibles: listar_secuencias, histograma, es_subsecuencia, enmascarar, guardar, salir, ayuda\n");
+        }
+        else{
+            if(argumento == "listar_secuencias"){
+                printf("Uso: listar_secuencias\n");
+                printf("Descripción: imprime la cantidad de secuencias y su información.\n");
+            }
+            else if(argumento == "histograma"){
+                printf("Uso: histograma <descripcion_secuencia>\n");
+                printf("Descripción: imprime la frecuencia de cada código en la secuencia, definida por cada salto de linea.\n");
+            }
+            else if(argumento == "es_subsecuencia"){
+                printf("Uso: es_subsecuencia <subsecuencia>\n");
+                printf("Descripción: Determina si una subsecuencia existe dentro de una secuencia ya cargada.\n");
+            }
+            else if(argumento == "enmascarar"){
+                printf("Uso: enmascarar <subsecuencia>\n");
+                printf("Descripción: Enmascara una subsecuencia dada por el usuario si existe.\n");
+            }
+            else if(argumento == "guardar"){
+                printf("Uso: guardar <nombre_archivo>\n");
+                printf("Descripción: guarda en el archivo las secuencias cargadas en memoria.\n");
+            }
+            else{
+                comandoNoReconocido();
+            }
+        }
+    }
+}
+
+
+// Mensajes para cuando el usuario ingresa un comando no reconocido
+void comandoNoReconocido(){
+    printf("Comando no reconocido. Escribe 'ayuda' para ver las opciones de comandos.\n");
+    printf("Si necesitas ayuda con un comando especifico, escribe 'ayuda <comando>'\n");
 }
